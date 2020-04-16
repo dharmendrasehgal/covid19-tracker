eb9e37d958e424e55f3e67785672d9f66286cb55
dharmendra-sehgal <dharmendra.sehgal@decorist.com>
2020-04-16 17:29:47 +0530

    Updates

---
+++ precache-manifest.7fe2c0f865b43b34923ce9003b699e35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aceabc6aa0ebaaea5944195448c11692",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "9aa89a321955e6197e79",
+    "url": "/covid19-tracker/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "8f6002cb5365606f4a77",
+    "url": "/covid19-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9aa89a321955e6197e79",
+    "url": "/covid19-tracker/static/js/2.63409580.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-tracker/static/js/2.63409580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6002cb5365606f4a77",
+    "url": "/covid19-tracker/static/js/main.0d673d57.chunk.js"
+  },
+  {
+    "revision": "dbae980a6809b37e52bb",
+    "url": "/covid19-tracker/static/js/runtime-main.56e53f11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
